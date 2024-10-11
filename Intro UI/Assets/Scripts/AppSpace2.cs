ec1cda951811a10c0d96ed2881e31853cb8869e2
Lucía <lucxx041@gmail.com>
2024-10-11 10:47:15 +0100

    Ejercicio 2
    Creadas las pantallas y principios de los scripts (asignaciones variables)

--- Intro UI/Assets/Scripts/AppSpace2.cs
+++ Intro UI/Assets/Scripts/AppSpace2.cs
@@ -1,9 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class AppSpace2 : MonoBehaviour
 {
+    [SerializeField]
+    float time = 15f;
+
+    [SerializeField]
+    float pulsaciones = 0;
+
+    [SerializeField]
+    float pMin = 0;
+
+    [SerializeField]
+    TextMeshProUGUI timeText;
+
+    [SerializeField]
+    TextMeshProUGUI nPulsaciones;
+
+    [SerializeField]
+    TextMeshProUGUI pulsacionesMin;
+
+
     void Start()
     {
         
@@ -11,6 +31,8 @@ public class AppSpace2 : MonoBehaviour
 
     void Update()
     {
-        
+        timeText.text = time.ToString();
+        nPulsaciones.text = pulsaciones.ToString();
+        pulsacionesMin.text = pMin.ToString();
     }
 }
